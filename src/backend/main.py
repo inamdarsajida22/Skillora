7e39ad0f9787b086116db8b721205f21428c50c5
Sajida Inamdar <inamdarsajida1@gmail.com>
2026-09-18 23:18:04 +0530

    Fix CORS for Vercel frontend

--- src/backend/main.py
+++ src/backend/main.py
@@ -24,10 +24,10 @@ app = FastAPI(title="Skillora API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-        "https://skillora-nu.vercel.app"
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://skillora-nu.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
